57b593c4be477336f707521f7f5fe6586dc4944a
NataliiaLuk <lukashevych.nat@gmail.com>
2024-03-18 21:42:53 +0100

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const list = document.querySelector('ul');
+
+const makeListElement = (elements) => {
+  return elements.map(element => {
+    const liElement = document.createElement('li');
+    liElement.textContent = element;
+    return (liElement);
+  });
+};
+
+const listElements = makeListElement(ingredients);
+list.append(...listElements);
+
