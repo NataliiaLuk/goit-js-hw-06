f1d9537bbf83177fd140a41c3d199d7a06a25cd0
NataliiaLuk <lukashevych.nat@gmail.com>
2024-03-18 21:42:49 +0100

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,16 @@
 const listCategories = document.querySelector('#categories');
-console.log(listCategories);
+const listItems = listCategories.querySelectorAll(".item");
+const listItemsArray = [...listItems];
+console.log('Number of categories:', listItemsArray.length);
+
+
+const showCategories = (categories) => {
+    return categories.forEach(category => {
+        const headingContent = category.querySelector('h2').textContent;
+        const itemQuantity = category.querySelectorAll('li').length;
+        console.log("Category:", headingContent);
+        console.log("Elements:", itemQuantity);
+    });
+};
+
+const categoriesNameElements = showCategories(listItemsArray);
