9683c7cf13df93a3430f5a53c9678ced8e3235f0
NataliiaLuk <lukashevych.nat@gmail.com>
2024-03-21 22:40:24 +0100

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -4,20 +4,22 @@ const destroy = buttons[1];
 const inputNumber = document.querySelector('input');
 console.log(inputNumber);
 
-const boxEl = document.createElement('div');
+// Якщо можна, будь ласка,дайте підказку чи рухаюсь в правильному напрямку тут
 
-function createBoxes(amount) {
-  for (let i = 0; i = amount; i += 1) {
-    const box = document.createElement('div');
-    box.style.color = getRandomHexColor();
-  }
-}
+// const boxEl = document.createElement('div');
+
+// function createBoxes(amount) {
+//   for (let i = 0; i = amount; i += 1) {
+//     const box = document.createElement('div');
+//     box.style.color = getRandomHexColor();
+//   }
+// }
 
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
 
-create.addEventListener('click', createBoxes(inputNumber));
+// create.addEventListener('click', createBoxes(inputNumber));
