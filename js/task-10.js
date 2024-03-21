b2690dc494dad20e7560f114d685fe85f1940ac5
NataliiaLuk <lukashevych.nat@gmail.com>
2024-03-21 22:32:24 +0100

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,23 @@
+const buttons = document.querySelectorAll('button');
+const create = buttons[0];
+const destroy = buttons[1];
+const inputNumber = document.querySelector('input');
+console.log(inputNumber);
+
+const boxEl = document.createElement('div');
+
+function createBoxes(amount) {
+  for (let i = 0; i = amount; i += 1) {
+    const box = document.createElement('div');
+    box.style.color = getRandomHexColor();
+  }
+}
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+create.addEventListener('click', createBoxes(inputNumber));
