d5bf27bced135fd9533f34cb95af2419595b2c30
NataliiaLuk <lukashevych.nat@gmail.com>
2024-03-21 22:32:17 +0100

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const colorName = document.querySelector('.color');
+const colorChanger = document.querySelector('.change-color');
+
+
+function onChangeColor(event) {
+  document.body.style.backgroundColor =  colorName.textContent = getRandomHexColor();
+  
+};
+
+colorChanger.addEventListener('click', onChangeColor);
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
