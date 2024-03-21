4daa7da05c659eb7fe5e75dc52a9191f6c7c7e19
NataliiaLuk <lukashevych.nat@gmail.com>
2024-03-21 22:32:13 +0100

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector('.login-form');
 
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const formElements = event.currentTarget.elements;
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+    const formData = {
+        mail,
+        password,
+    };
+    if (mail === '' || password === '') {
+        alert("All fields must be filled in");
+    } else {
+        console.log(formData);
+    }
+    event.currentTarget.reset();
+};
+
+form.addEventListener('submit', onFormSubmit);
